c7cdd4757d2c0ac791d59b7794a7958662434fdc
Lorenc, Vaclav <vlorenc@netsuite.com>
2020-01-09 17:52:23 +0100

    Temp commit

--- provision/provision.sh
+++ provision/provision.sh
@@ -6,17 +6,19 @@ sudo rpm -Uvh http://repository.it4i.cz/mirrors/repoforge/redhat/el7/en/x86_64/r
 sudo yum updateinfo
 
 # Install the things that are available as packages, and remove things that will be replaced by PIP
-sudo yum install -y vim wget python-pip gcc gcc-c++ python-devel readline-devel patch capstone git python3 python3-devel python-virtualenv radare2
+sudo yum install -y vim wget python-pip gcc gcc-c++ python-devel readline-devel patch capstone git python3 python3-devel python-virtualenv radare2 strace
 sudo yum remove -y PyYAML
 sudo pip install powerline-status distorm3
 sudo yum install -y bash-completion bash-completion-extras
-sudo yum install -y python2-volatility foremost unzip
+sudo yum install -y python2-volatility foremost unzip yara yara-dev python2-yara python3-yara
+sudo yum install -y git
 
 # Prepare the VirtualEnv for Google Rekall. This allows us to use the latest available/functional version,
 # and to use Python3
 virtualenv ~/.venv/ --python python3
 source ~/.venv/bin/activate
-pip install --upgrade pip setuptools pyasn1 pyyaml wheel
+ln -s /usr/lib/libyara.so /home/vagrant/.venv/lib/libyara.so
+pip install --upgrade pip setuptools pyasn1 pyyaml wheel yara
 pip install future==0.16.0 pyaff4==0.26.post6
 pip install rekall rekall-agent
 deactivate
@@ -24,6 +26,14 @@ deactivate
 # Prepare some further folder structure for our work
 mkdir ~/bin/
 mkdir ~/images/
+
+# Prepare some Yara signatures
+mkdir ~/yara/
+(
+  cd ~/yara/
+  git clone https://github.com/Neo23x0/signature-base
+  git clone https://github.com/Yara-Rules/rules
+)
 
 # Final touches
 ln -s ~/.venv/bin/rekal ~/bin/rekal
